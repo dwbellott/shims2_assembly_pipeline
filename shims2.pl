0d7f5d05b72a1bceb1d7a8af454a49db0586f13a
Daniel W Bellott <bellott@wi.mit.edu>
2018-01-23 16:50:11 -0500

    bump version and remove small contigs

--- shims2.pl
+++ shims2.pl
@@ -56,7 +56,7 @@ use vars qw/@temporary/;
 
 
 BEGIN {
-	$VERSION = '1.1.27';
+	$VERSION = '1.1.28';
 	$spades_default = $ENV{'SHIMS_SPADES_EXEC'} || which('spades.py');
 	$samtools_default = $ENV{'SHIMS_SAMTOOLS_EXEC'} || which('samtools');
 	$bowtie2build_default = $ENV{'SHIMS_BOWTIE2BUILD_EXEC'} || which('bowtie2-build');
@@ -88,7 +88,7 @@ sub make_illumina_arguments ($$$);
 sub merge_fastas($$$);
 sub screen_pacbio ($$$$);
 sub imagine ($$$$$$);
-sub write_fasta_sequences ($$);
+sub write_fasta_sequences ($$$);
 sub quality_trim_contigs ($$);
 sub load_fasta_contigs ($);
 sub overlap_mates ($$$$$$);
@@ -503,7 +503,7 @@ sub main() {
 	#load up the output of BESST and Gap2Seq and write in the output folder
 
 	my $contigs = load_fasta_contigs($scaffolds);
-	write_fasta_sequences($contigs, $final);
+	write_fasta_sequences($contigs, $final, $avg_read);
 	my $input_contigs = load_fasta_contigs($final);
 
 	#if there are end sequences, try to order and orient remaining contigs
@@ -600,17 +600,19 @@ sub estimate_coverage_cutoff_from_vector ($$$) {
 
 #uses bioperl to write sequences in fasta format
 
-sub write_fasta_sequences ($$){
-	my ($contigs, $file) = @_;
-	my $out = Bio::SeqIO->new(-file => ">$file", '-format' => 'Fasta');
-	my $n = 0;
-	foreach my $id (keys(%$contigs)){
-		$out->write_seq($contigs->{$id});
-		$n++
-	}
-	return $n;
+sub write_fasta_sequences ($$$){
+        my ($contigs, $file, $avg_read_len) = @_;
+        my $out = Bio::SeqIO->new(-file => ">$file", '-format' => 'Fasta');
+        my $n = 0;
+        foreach my $id (keys(%$contigs)){
+                #get rid of 128bp junk contigs
+                unless ($contigs->{$id}->length() < $avg_read_len){
+                        $out->write_seq($contigs->{$id});
+                        $n++
+                }
+        }
+        return $n;
 }
-
 
 #loads a fasta file
 
